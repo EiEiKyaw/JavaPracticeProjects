a799338436b7c8a136a0f3e1ed4fed6f73a7cf92
Win Lwin <143885612+Ko-Win-Lwin@users.noreply.github.com>
2024-02-28 10:20:48 +0630

    Group Switch

---
+++ src/main/java/winlwin/com/switchcase/Holiday.java
@@ -0,0 +1,38 @@
+package winlwin.com.switchcase;
+
+import java.time.LocalDate;
+
+public class Holiday {
+	
+	static void checkHoliday(String day) {
+		day = day.toLowerCase();
+		
+		switch (day) {
+		case "monday":
+			System.out.println("Ahh shit. here we go again!");
+			break;
+			
+		case "tuesday", "wednesday", "thursday", "friday":
+			System.out.println("Weekdays");
+			break;
+			
+		case "saturday", "sunday":
+			System.out.println("Tadaa. Today is Holiday");
+			break;
+		}
+	}
+	
+	static void testCheckHoliday(String day) {
+		checkHoliday(day);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String day = LocalDate.now().getDayOfWeek().name().toString();
+		testCheckHoliday("saturday");
+		
+		
+		
+	}
+
+}
